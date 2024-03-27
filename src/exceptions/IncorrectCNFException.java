75ca2c94afb83cfd5982d2e751e0ada592d9720e
Mikhael fS <82028882+michael2024-lw@users.noreply.github.com>
2024-03-27 08:51:24 +0000

    readme

--- src/exceptions/IncorrectCNFException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class IncorrectCNFException extends Exception {
-    public IncorrectCNFException(String message) {
-        super(message);
-    }
-}
--- src/services/CNFModelCheckerService.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-import repositories.model.ModelCheckResult;
-
-import java.util.List;
-
-public interface CNFModelCheckerService {
-    List<ModelCheckResult> getModelCheckingResult(String configPath, boolean computeBackwards);
-}
